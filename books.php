f7c2695b7549fc8ca9abc56c9262d94e54d1e378
Porcupine92 <47852549+Porcupine92@users.noreply.github.com>
2019-10-17 19:40:58 +0200

    Create books.php

---
+++ books.php
@@ -0,0 +1,170 @@
+<?php include_once('include/include.php');?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Books</title>
+        <meta CHARSET="UTF-8"/>
+		<link rel="stylesheet" href="main.css"/>
+    </head>
+    <body>
+		<table class="table">
+			<thead>
+				<tr>
+					<th>
+						Book
+					</th>
+					<th>
+						Compatibility
+					</th>
+					<th>
+						Book Date
+					</th>
+					<th>
+						Female AVG age
+					</th>
+					<th>
+						Male AVG age
+					</th>
+				</tr>
+			</thead>
+			<tbody>
+		<?php
+			$book='ZieLoNa MiLa';
+			$sql="SELECT b.id, b.name, b.book_date, r.book_id, r.age, r.sex 
+				FROM books b
+				INNER JOIN reviews r ON r.book_id=b.id
+				WHERE b.name='$book'
+				AND r.age>=30";
+			$result=mysqli_query($conn,$sql);
+			$fAge=array();
+			$mAge=array();
+			
+			while($row=mysqli_fetch_array($result)){
+				$name=$row['name'];
+				$date=$row['book_date'];
+				$name=strtolower($name);
+				$book=strtolower($book);
+				$sim = similar_text($name, $book, $perc);
+				$comp=round($perc, 2);
+					if(($row['sex'])=='f'){
+						$fAge[]=$row['age'];
+					};
+					if(($row['sex'])=='m'){
+						$mAge[]=$row['age'];
+					};
+					$name=$row['name'];
+				
+			}
+				echo '<tr>';
+					echo '<td>';
+						echo $name;
+					echo '</td>';
+					echo '<td>';
+						echo $comp.'%';
+					echo '</td>';
+					echo '<td>';
+						echo $date;
+					echo '</td>';
+					echo '<td>';
+						if(count($fAge)==0){
+							echo '0';
+						}else{
+							$avg=round((array_sum($fAge)/count($fAge)),2);
+							echo $avg;
+						}
+					echo '</td>';
+					echo '<td>';
+						if(count($mAge)==0){
+							echo '0';
+						}else{
+							$avg=round((array_sum($mAge)/count($mAge)),2);
+							echo $avg;
+						};
+					echo '</td>';
+				echo '</tr>';
+			echo '</tbody>';
+		echo '</table>';
+		?>
+		<table class="table">
+			<thead>
+				<tr>
+					<th>
+						Book
+					</th>
+					<th>
+						Compatibility
+					</th>
+					<th>
+						Book Date
+					</th>
+					<th>
+						Female AVG age
+					</th>
+					<th>
+						Male AVG age
+					</th>
+				</tr>
+			</thead>
+			<tbody>
+		<?php 
+			
+			$book='ZiElonA Droga';
+			$sql="SELECT DISTINCT b.id, b.name, b.book_date, r.book_id, r.age, r.sex 
+			FROM books b
+			INNER JOIN reviews r ON r.book_id=b.id
+			WHERE r.age<30
+			GROUP BY b.name";
+			$result=mysqli_query($conn,$sql);
+			//vd($result);
+			$fAge=array();
+			$mAge=array();
+			while($row=mysqli_fetch_array($result)){
+				if($book!==$row['name']){
+				$name=$row['name'];
+				$date=$row['book_date'];
+				$name=strtolower($name);
+				$book=strtolower($book);
+				$sim = similar_text($name, $book, $perc);
+				$comp=round($perc, 2);
+					if(($row['sex'])=='f'){
+						$fAge[]=$row['age'];
+					};
+					if(($row['sex'])=='m'){
+						$mAge[]=$row['age'];
+					};
+				$name=$row['name'];
+				
+				echo '<tr>';
+					echo '<td>';
+						echo $name;
+					echo '</td>';
+					echo '<td>';
+						echo $comp.'%';
+					echo '</td>';
+					echo '<td>';
+						echo $date;
+					echo '</td>';
+					echo '<td>';
+						if(count($fAge)==0){
+							echo '0';
+						}else{
+							$avg=round((array_sum($fAge)/count($fAge)),2);
+							echo $avg;
+						}
+					echo '</td>';
+					echo '<td>';
+						if(count($mAge)==0){
+							echo '0';
+						}else{
+							$avg=round((array_sum($mAge)/count($mAge)),2);
+							echo $avg;
+						};
+					echo '</td>';
+				echo '</tr>';
+				}
+			};
+			echo '</tbody>';
+		echo '</table>';
+		?>
+	</body>
+</html>
